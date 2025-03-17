db72322606671759fce4d8de3eb5a801a036808e
Vladimir Barashko <general@vbarashko.com>
2025-03-17 01:26:53 +0100

    Updates

---
+++ precache-manifest.b10b8e5ea9b50ee69134ce7ffacc7d7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b025d68299c75f03d61538855a7c0532",
+    "url": "/pega-tracer-event-perf-visualizer/index.html"
+  },
+  {
+    "revision": "628442921b334d3e4d54",
+    "url": "/pega-tracer-event-perf-visualizer/static/css/main.fb37193e.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/pega-tracer-event-perf-visualizer/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "628442921b334d3e4d54",
+    "url": "/pega-tracer-event-perf-visualizer/static/js/main.5ada4df7.chunk.js"
+  },
+  {
+    "revision": "2eb60e7db299e042780f",
+    "url": "/pega-tracer-event-perf-visualizer/static/js/runtime~main.3ca02db0.js"
+  }
+]);
